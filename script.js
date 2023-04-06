d0342f7c10eba7ae0eec9e6c9537f79e097bc089
Aleksandra Ujvari <ujvari65@gmail.com>
2023-04-06 21:55:02 +0200

    Add footer section and css for footer

--- script.js
+++ script.js
@@ -44,3 +44,18 @@ $(document).ready(function () {
 
   setInterval(slide, interval);
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myTopBtn").style.display = "block";
+  } else {
+    document.getElementById("myTopBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
