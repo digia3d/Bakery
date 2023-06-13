4d830cc026235464b7954d8de21b3bd7cd2a37dc
Aleksandra Ujvari <ujvari65@gmail.com>
2023-06-13 23:23:27 +0200

    Fix linters errors

--- script.js
+++ script.js
@@ -1,63 +1,63 @@
 /* global $ */
 
 document.addEventListener('DOMContentLoaded', () => {
-const mainbody = document.querySelector('.mainbody');
-const menuList = document.querySelector('.list');
-const hamburger = document.querySelector('.hamburger');
-const closeIcon = document.querySelector('.menu-closebtn .times');
-const menuItems = Array.from(document.querySelectorAll('.item'));
+  const mainbody = document.querySelector('.mainbody');
+  const menuList = document.querySelector('.list');
+  const hamburger = document.querySelector('.hamburger');
+  const closeIcon = document.querySelector('.menu-closebtn .times');
+  const menuItems = Array.from(document.querySelectorAll('.item'));
 
-hamburger.addEventListener('click', () => {
-  menuList.classList.add('active');
-  closeIcon.classList.add('icon');
-});
+  hamburger.addEventListener('click', () => {
+    menuList.classList.add('active');
+    closeIcon.classList.add('icon');
+  });
 
-menuItems.forEach((menuItem) => {
-  menuItem.addEventListener('click', () => {
-    menuItems.forEach((Item) => { Item.classList.remove('active'); });
-    menuList.classList.remove('active');
-    menuItem.classList.add('active');
+  menuItems.forEach((menuItem) => {
+    menuItem.addEventListener('click', () => {
+      menuItems.forEach((Item) => { Item.classList.remove('active'); });
+      menuList.classList.remove('active');
+      menuItem.classList.add('active');
 
-    if (menuItem.textContent === 'home' && !mainbody.classList.contains('homepage')) {
-      localStorage.setItem(menuItem.textContent, 'active');
+      if (menuItem.textContent === 'home' && !mainbody.classList.contains('homepage')) {
+        localStorage.setItem(menuItem.textContent, 'active');
+      }
+    });
+  });
+
+  window.addEventListener('resize', () => {
+    if (mainbody.clientWidth >= 768 && menuList.classList.contains('active')) {
+      window.location.reload();
     }
   });
-});
 
-window.addEventListener('resize', () => {
-  if (mainbody.clientWidth >= 768 && menuList.classList.contains('active')) {
-    window.location.reload();
-  }
-});
+  $(document).ready(() => {
+    const interval = 3000; // set the interval time in milliseconds
+    const slides = $('.slide');
+    let currentIndex = 0;
+    const maxIndex = slides.length - 1;
 
-$(document).ready(() => {
-  const interval = 3000; // set the interval time in milliseconds
-  const slides = $('.slide');
-  let currentIndex = 0;
-  const maxIndex = slides.length - 1;
-
-  function slide() {
-    slides.eq(currentIndex).fadeOut(500);
-    currentIndex += 1;
-    if (currentIndex > maxIndex) {
-      currentIndex = 0;
+    function slide() {
+      slides.eq(currentIndex).fadeOut(500);
+      currentIndex += 1;
+      if (currentIndex > maxIndex) {
+        currentIndex = 0;
+      }
+      slides.eq(currentIndex).fadeIn(500);
     }
-    slides.eq(currentIndex).fadeIn(500);
-  }
 
-  setInterval(slide, interval);
-});
+    setInterval(slide, interval);
+  });
 
-function scrollFunction() {
-  const myTopBtn = document.getElementById('myTopBtn');
-  if (myTopBtn !== null) {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      myTopBtn.style.display = 'block';
-    } else {
-      myTopBtn.style.display = 'none';
+  function scrollFunction() {
+    const myTopBtn = document.getElementById('myTopBtn');
+    if (myTopBtn !== null) {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myTopBtn.style.display = 'block';
+      } else {
+        myTopBtn.style.display = 'none';
+      }
     }
   }
-}
 
   window.onscroll = function () { scrollFunction(); };
 });
